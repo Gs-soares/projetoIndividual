ecad743e377e5381e869001465f9f3bbd651e0f1
0BlackSmoke0 <bertucci.henrique@gmail.com>
2020-11-18 15:28:20 -0300

    atualizando projeto individual

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,18 @@
 function verificar(){
     alert('Não ta pronto ainda AAAAA')
 }
+
+function openModal(){
+   var openLogin = document.getElementById('loginGames');
+   openLogin.style.height= '80vh';
+   openLogin.style.backgroundColor = 'black';
+   openLogin.style.width = '90%';
+   openLogin.style.zIndex = '2';
+   openLogin.style.position = 'fixed';
+   openLogin.style.textAlign = 'center';
+   openLogin.style.transform = 'translate(-50%, -50%)';
+   openLogin.style.top = '50%';
+   openLogin.style.left = '50%';
+   openLogin.style.borderRadius = '15px';
+}
+
