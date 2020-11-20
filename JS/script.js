ba97a434fb135f70d24f71751bd561351cd8e362
Gs-soares <guilherme.castro@bandtec.com.br>
2020-11-19 23:42:46 -0300

    atualizando o projeto individual

--- JS/script.js
+++ JS/script.js
@@ -1,31 +1,26 @@
-function verificar(){
+function verificar() {
     alert('Não ta pronto ainda AAAAA')
 }
 
-function openModal(){
-   var openLogin = document.getElementById('loginGames');
-   openLogin.style.height= '90vh';
-   openLogin.style.background = 'rgb(40,120,189)';
-   openLogin.style.background = 'linear-gradient(90deg, rgba(40,120,189,1) 0%, rgba(29,233,253,1) 50%, rgba(69,252,188,1) 100%)';
-   openLogin.style.width = '40%';
-   openLogin.style.zIndex = '2';
-   openLogin.style.position = 'fixed';
-//    openLogin.style.textAlign = 'center';
-   openLogin.style.transform = 'translate(-50%, -50%)';
-   openLogin.style.top = '50%';
-   openLogin.style.left = '50%';
-   openLogin.style.borderRadius = '15px';
-   openLogin.style.display = 'block';
-   document.getElementById('entrar').style.display= 'block';
-//    document.getElementById('cadastro').style.display= 'block';
-//    document.getElementById('formulario').style.display= 'block';
+function openModal() {
+    var openLogin = document.getElementById('loginGames');
+    openLogin.style.height = '75vh';
+    openLogin.style.background = 'rgb(40,120,189)';
+    openLogin.style.background = 'linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(54,70,70,0.9051217899269083) 43%, rgba(56,56,56,0.9443374762014181) 59%, rgba(55,55,54,1) 100%)';
+    openLogin.style.width = '30%';
+    openLogin.style.zIndex = '2';
+    openLogin.style.position = 'fixed';
+
+    openLogin.style.transform = 'translate(-50%, -50%)';
+    openLogin.style.top = '50%';
+    openLogin.style.left = '50%';
+    openLogin.style.borderRadius = '15px';
+    openLogin.style.display = 'block';
+    document.getElementById('entrar').style.display = 'block';
 }
 
-function closeModal(){
-    var openLogin = document.getElementById('loginGames');       
-    document.getElementById('entrar').style.display= 'none';
-   document.getElementById('cadastro').style.display= 'none';
-   document.getElementById('formulario').style.display= 'none';
-    openLogin.style.display = 'none';
-    openLogin.style.height= '0';
+function closeModal() {
+    var closeLogin = document.getElementById('loginGames');
+    closeLogin.style.display = 'none';
+    closeLogin.style.height = '0';
 }
